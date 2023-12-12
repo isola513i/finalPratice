21e57956467272abe0bdfa430791372081fe29e7
Nattapat Lamnui <okuma530@gmail.com>
2023-12-13 01:00:57 +0700

    update-Exercise

---
+++ src/Exercise/Exercise2.java
@@ -0,0 +1,12 @@
+package Exercise;
+
+public class Exercise2 {
+  public static void main(String[] args) {
+    int [] number = {10,20,30,40};
+    int sum = 0;
+    for (int i : number){
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
